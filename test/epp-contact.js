39daa623f1dcfcdec78a3a48e3a40b54d72d32ef
Travis Holton <travis@catalyst.net.nz>
2014-08-22 14:07:43 +1200

    create a contact

--- test/epp-contact.js
+++ test/epp-contact.js
@@ -1,11 +1,63 @@
-
 nconf = require('nconf');
 nconf.env().file({
-    "file": "./config/epp-reg.json"
+	"file": "./config/epp-reg-devel.json"
 });
 var fs = require('fs');
 var chai = require('chai');
 var moment = require('moment');
+var AMQP = require('amqp-as-promised');
 
 var expect = chai.expect,
 should = chai.should;
+
+var rabbitmqConfig = nconf.get('rabbitmq');
+
+describe("Contact creation", function() {
+	var amqpConnection;
+	before(function() {
+		amqpConnection = new AMQP(rabbitmqConfig.connection);
+	});
+	it('should create a contact', function(done) {
+		var contactId = ['iwmn', moment().unix()].join('-');
+
+		var contactData = {
+			"id": contactId,
+			"voice": "+1.9405551234",
+			"fax": "+1.9405551233",
+			"email": "john.doe@null.com",
+			"authInfo": {
+				"pw": "xyz123"
+			},
+			"postalInfo": [{
+				"name": "John Doe",
+				"org": "Example Ltd",
+				"type": "int",
+				"addr": [{
+					"street": ["742 Evergreen Terrace", "Apt b"],
+					"city": "Springfield",
+					"sp": "OR",
+					"pc": "97801",
+					"cc": "US"
+				}]
+			}]
+		};
+		var createContact = {
+			"command": "createContact",
+			"data": contactData
+		};
+
+		amqpConnection.rpc('epp', 'eppServer.hexonet-test1', createContact, null, {
+			"timeout": 4000
+		}).then(function(data) {
+			try {
+				expect(data).to.have.deep.property('result');
+				done();
+			} catch(e) {
+				done(e);
+			}
+		});
+
+	});
+
+});
+
