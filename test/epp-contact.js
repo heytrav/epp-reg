9fa87ee999244cfa5c6d12a49d8417ade81da7a3
Travis Holton <travis@catalyst.net.nz>
2014-08-22 15:15:59 +1200

    create a class to manage the command request

--- test/epp-contact.js
+++ test/epp-contact.js
@@ -1,22 +1,28 @@
 nconf = require('nconf');
-nconf.env().file({
-	"file": "./config/epp-reg-devel.json"
-});
 var fs = require('fs');
 var chai = require('chai');
 var moment = require('moment');
-var AMQP = require('amqp-as-promised');
 
 var expect = chai.expect,
 should = chai.should;
 
-var rabbitmqConfig = nconf.get('rabbitmq');
+var EppCommander = require('../lib/epp-commander.js');
+
 
 describe("Contact creation", function() {
-	var amqpConnection;
+    var eppCommander;
 	before(function() {
-		amqpConnection = new AMQP(rabbitmqConfig.connection);
+        eppCommander = new EppCommander('hexonet-test1');
 	});
+    var processResult = function(data) {
+        if (data.result && data.result.code) {
+            var code = data.result.code;
+            if (code >= 2000) {
+                throw data;//new Error("EPP Exception");
+            }
+        }
+        return data;
+    };
 	it('should create a contact', function(done) {
 		var contactId = ['iwmn', moment().unix()].join('-');
 
@@ -41,23 +47,59 @@ describe("Contact creation", function() {
 				}]
 			}]
 		};
-		var createContact = {
-			"command": "createContact",
-			"data": contactData
-		};
 
-		amqpConnection.rpc('epp', 'eppServer.hexonet-test1', createContact, null, {
-			"timeout": 4000
-		}).then(function(data) {
+		eppCommander.createContact(contactData).then(function (successfulResult) {
+            return processResult(successfulResult);
+        }).then(function(data){
 			try {
 				expect(data).to.have.deep.property('result');
 				done();
 			} catch(e) {
 				done(e);
-			}
-		});
+			}}, function (error) {
+                console.error("This shouldn't happen");
+            });
 
 	});
 
+    it('should get a failed create contact error', function(done) {
+		var contactId = ['iwmn', moment().unix()].join('-');
+        
+		var contactData = {
+			"id": 'auto', // bogus id
+			"voice": "+1.9405551234",
+			"fax": "+1.9405551233",
+			"email": "john.doe@null.com",
+			"authInfo": {
+				"pw": "xyz123"
+			},
+			"postalInfo": [{
+				"name": "John Doe",
+				"org": "Example Ltd",
+				"type": "int",
+				"addr": [{
+					"street": ["742 Evergreen Terrace", "Apt b"],
+					"city": "Springfield",
+					"sp": "OR",
+					"pc": "97801",
+					"cc": "US"
+				}]
+			}]
+		};
+
+		eppCommander.createContact(contactData).then(function(data) {
+            return processResult(data);
+		}).then(function (data) {
+            console.log("Successful result shouldn't happen");
+        }, function (error) {
+			try {
+				expect(error).to.have.deep.property('result');
+                expect(error.result.code).to.be.gt(2000);
+				done();
+			} catch(e) {
+				done(e);
+			}
+        });
+    });
 });
 
